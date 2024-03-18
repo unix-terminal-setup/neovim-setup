9707cec180066e06b4edbd960b08e3e1dd7b7bc0
Coenraad Human <coen.human@gmail.com>
2024-03-18 14:49:47 +0200

    fix: pipeline

--- setup.sh
+++ setup.sh
@@ -18,6 +18,4 @@ echo "Adding an alias for nvim to vim"
 echo 'alias vim="nvim"' >>~/.bashrc
 
 # Test installation was successful:
-nvim --help || echo "Failed installing NeoVim" && exit 1
-
-echo "Successfully install NeoVim"
+nvim --help | echo "Successfully install NeoVim"
